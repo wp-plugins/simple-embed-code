12db5628e44211e12582eb446d0dc5571bf791fa
dartiss <dartiss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-22 20:20:50 +0000

    git-svn-id: https://plugins.svn.wordpress.org/simple-embed-code/tags/1.6.1@442316 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ simple-code-embed.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Plugin Name: Artiss Code Embed
+Plugin URI: http://www.artiss.co.uk/code-embed
+Description: Allows you to embed code into your posts & pages
+Version: 1.6.1
+Author: David Artiss
+Author URI: http://www.artiss.co.uk
+*/
+
+/**
+* Artiss Code Embed
+*
+* Embed code into a post
+*
+* @package	Artiss-Code-Embed
+* @since	1.6
+*/
+
+define( 'artiss_code_embed_version', '1.6.1' );
+
+$functions_dir = WP_PLUGIN_DIR . '/simple-embed-code/includes/';
+
+// Include all the various functions
+
+include_once( $functions_dir . 'get-options.php' );							// Get the default options
+
+if ( is_admin() ) {
+
+	include_once( $functions_dir . 'admin-menu.php' );						// Administration menus
+
+	if ( !has_action( 'wp_dashboard_setup', 'artiss_dashboard_widget' ) ) {
+
+		include_once( $functions_dir . 'artiss-dashboard-widget.php' );		// Artiss dashboard widget
+
+	}
+
+} else {
+
+	include_once( $functions_dir . 'code-embed-filter.php' );				// Filter to apply code embeds
+
+}
+?>
